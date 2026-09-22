25ee8c46de8366be61af4ab00d5a750f71fa355d
Mist <cryptalmist@gmail.com>
2026-09-22 08:11:21 +0700

    Fix runtime: drop getKey accessor (vanilla method exists), drop refmap (unneeded, matches MaFgLib)

--- src/main/java/dev/velolib/radial/mixin/KeyMappingAccessor.java
+++ src/main/java/dev/velolib/radial/mixin/KeyMappingAccessor.java
@@ -1,6 +1,5 @@
 package dev.velolib.radial.mixin;
 
-import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
@@ -12,7 +11,4 @@ public interface KeyMappingAccessor {
 
     @Accessor("clickCount")
     void setClickCount(int timesPressed);
-
-    @Accessor("key")
-    InputConstants.Key getKey();
 }
